baa847549fa95d4f6f28c3091abeab36e541ae43
Nahid Hasan <nahidjc80@gmail.com>
2023-06-06 17:49:41 +0600

    design appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:rnd_flutter_app/pages/app_drawer.dart';
 import 'package:rnd_flutter_app/pages/qr_code_widget.dart';
 import 'package:rnd_flutter_app/pages/transaction_history.dart';
 import 'package:rnd_flutter_app/pages/user_profile.dart';
@@ -67,10 +68,15 @@ class _HomePageState extends State<HomePage> {
   }
 
   int _currentIndex = 0;
-
+final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        key: _scaffoldKey,
+        endDrawer: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.5,
+          child: AppDrawer(),
+        ),
         appBar: AppBar(
           toolbarHeight: 70, // set the height of the AppBar
           backgroundColor: Colors.white,
@@ -104,7 +110,7 @@ class _HomePageState extends State<HomePage> {
             IconButton(
               icon: const Icon(Icons.menu),
               onPressed: () {
-                // Handle menu icon tap
+                _scaffoldKey.currentState!.openEndDrawer();
               },
             ),
             // IconButton(icon: const Icon(Icons.search), onPressed: () {})
@@ -267,7 +273,7 @@ class _HomePageState extends State<HomePage> {
                       color: Colors.grey.withOpacity(0.1),
                       spreadRadius: 1,
                       blurRadius: 2,
-                      offset: const Offset(0, 1), 
+                      offset: const Offset(0, 1),
                     ),
                   ],
                 ),
@@ -279,12 +285,12 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false, 
+          showSelectedLabels: false,
           showUnselectedLabels: false,
-          currentIndex: _currentIndex, 
+          currentIndex: _currentIndex,
           onTap: (int index) {
             setState(() {
-              _currentIndex = index; 
+              _currentIndex = index;
               if (index == 1) {
                 Navigator.of(context).push(MaterialPageRoute(
                   builder: (context) => const QRViewExample(),
